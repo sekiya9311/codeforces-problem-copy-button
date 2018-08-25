eb7b12266a6c592189fb0b1d2ebb594c02562c6e
sekiya9311 <sekiya9311@gmail.com>
2018-08-25 17:04:10 +0900

    userscript生成用のタスクと生成に必要なファイル作成

---
+++ UserScriptHeader.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        codeforces-problem-copy-button
+// @namespace   https://sekiya9311.github.io/
+// @version     1.0.0
+// @description Copy the problem sentence of current page.
+// @author      sekiya9311
+// @supportURL  https://github.com/sekiya9311/codeforces-problem-copy-button/issues
+// @include     https://codeforces.com/*
+// @include     http://codeforces.com/*
+// ==/UserScript==
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const fs = require('fs');
+const webpack = require('webpack');
+const WebpackStream = require('webpack-stream');
+
+const WebpackConfig = require('./webpack.config');
+const UserScriptHeader = fs.readFileSync('./UserScriptHeader.js', 'utf8');
+
+const WebpackTaskName = 'webpack';
+const GenerateUserscriptTaskName = 'generate-userscript';
+const DefaultTaskName = 'default';
+
+gulp.task(WebpackTaskName, () => {
+  return WebpackStream(WebpackConfig, webpack).pipe(gulp.dest('./build/'));
+});
+
+gulp.task(GenerateUserscriptTaskName, gulp.series(WebpackTaskName, (done) => {
+  const convertedSourceOfWebpack = fs.readFileSync('./build/bundle.js', 'utf8');
+  fs.writeFileSync('./build/codeforces-problem-copy-button.user.js', 
+                   UserScriptHeader + '\n' + convertedSourceOfWebpack);
+  done();
+}));
+
+gulp.task(DefaultTaskName, gulp.series(GenerateUserscriptTaskName, (done) => done()));
