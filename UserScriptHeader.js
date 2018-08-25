cbc5fb14e404469aee7ff686d4bbff0834f8a89b
sekiya9311 <sekiya9311@gmail.com>
2018-08-26 02:58:52 +0900

    ヘッダ編集

--- UserScriptHeader.js
+++ UserScriptHeader.js
@@ -2,7 +2,7 @@
 // @name        codeforces-problem-copy-button
 // @namespace   https://sekiya9311.github.io/
 // @version     1.0.0
-// @description Copy the problem sentence of current page.
+// @description Copy the problem sentence of current Codeforces page.
 // @author      sekiya9311
 // @supportURL  https://github.com/sekiya9311/codeforces-problem-copy-button/issues
 // @updateURL   https://raw.githubusercontent.com/sekiya9311/codeforces-problem-copy-button/master/build/codeforces-problem-copy-button.user.js
