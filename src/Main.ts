693154881c543f7cd7405ea94242c74ddc1d6a67
sekiya9311 <sekiya9311@gmail.com>
2018-08-26 22:31:14 +0900

    処理の順序の変更

--- src/Main.ts
+++ src/Main.ts
@@ -4,63 +4,68 @@ export class Main {
   private readonly NewLine: string = '\n';
 
   public constructor() {
-    const problemStates = document.getElementsByClassName(this.ProblemStatementId);
-    let convertProblem = new Array<string>();
-    if (problemStates !== null && problemStates.length > 0) {
-      try {
-        const problemState = problemStates[0];
-
-        // head
-        const headerElement = problemState.children[0] as HTMLElement;
-        {
-          let convert = headerElement.innerText;
-          convertProblem.push(convert.replace('time limit per test', 'time limit per test: ')
-            .replace('memory limit per test', 'memory limit per test: ')
-            .replace('inputstandard input', 'input: standard input')
-            .replace('outputstandard output', 'output: standard output'));
-        }
-
-        // body
-        if (problemState.children.length > 1) {
-          const bodyElement = problemState.children[1] as HTMLElement;
-          convertProblem.push(this.analyzeElement(bodyElement));
-        }
-
-        // input format
-        if (problemState.children.length > 2) {
-          const inputElement = problemState.children[2] as HTMLElement;
-          convertProblem.push(this.analyzeElement(inputElement));
-        }
-
-        // output format
-        if (problemState.children.length > 3) {
-          const outputElement = problemState.children[3] as HTMLElement;
-          convertProblem.push(this.analyzeElement(outputElement));
-        }
-
-        // sample (do nothing)
-        if (problemState.children.length > 4) {
-          const sampleElement = problemState.children[4] as HTMLElement;
-        }
-
-        // note
-        if (problemState.children.length > 5) {
-          const noteElement = problemState.children[5] as HTMLElement;
-          convertProblem.push(this.analyzeElement(noteElement));
-        }
-
-        if (this.setCopyButton(convertProblem.join(this.NewLine).split('$$$').join(''))) {
-          this.putLog('Set button success!');
-        } else {
-          throw new Error('Set button failed...');
-        }
-      } catch (err) {
-        this.putLog('Set button failed...', err);
-      }
+    if (this.setCopyButton()) {
+      this.putLog('Set button success!');
     } else {
-      this.putLog('Not problem page.');
+      throw new Error('Set button failed...');
     }
   }
+
+private getProblem(): string {
+  const problemStates = document.getElementsByClassName(this.ProblemStatementId);
+  let convertProblem = new Array<string>();
+  if (problemStates !== null && problemStates.length > 0) {
+    try {
+      const problemState = problemStates[0];
+
+      // head
+      const headerElement = problemState.children[0] as HTMLElement;
+      {
+        let convert = headerElement.innerText;
+        convertProblem.push(convert.replace('time limit per test', 'time limit per test: ')
+          .replace('memory limit per test', 'memory limit per test: ')
+          .replace('inputstandard input', 'input: standard input')
+          .replace('outputstandard output', 'output: standard output'));
+      }
+
+      // body
+      if (problemState.children.length > 1) {
+        const bodyElement = problemState.children[1] as HTMLElement;
+        convertProblem.push(this.analyzeElement(bodyElement));
+      }
+
+      // input format
+      if (problemState.children.length > 2) {
+        const inputElement = problemState.children[2] as HTMLElement;
+        convertProblem.push(this.analyzeElement(inputElement));
+      }
+
+      // output format
+      if (problemState.children.length > 3) {
+        const outputElement = problemState.children[3] as HTMLElement;
+        convertProblem.push(this.analyzeElement(outputElement));
+      }
+
+      // sample (do nothing)
+      if (problemState.children.length > 4) {
+        const sampleElement = problemState.children[4] as HTMLElement;
+      }
+
+      // note
+      if (problemState.children.length > 5) {
+        const noteElement = problemState.children[5] as HTMLElement;
+        convertProblem.push(this.analyzeElement(noteElement));
+      }
+
+      return convertProblem.join(this.NewLine).split('$$$').join('');
+      
+    } catch (err) {
+      throw err;
+    }
+  } else {
+    throw new Error('Not problem page.');
+  }
+}
 
   private analyzeElement(element: HTMLElement): string {
     let result = new Array<string>();
@@ -129,41 +134,23 @@ export class Main {
           }
           break;
         }
-        case 'mn': {
-          let foo = this.analyzeElement(childNode as HTMLElement);
-          if (foo.length) {
-            result.push(foo);
-          }
-          break;
-        }
-        case 'mi': {
-          let foo = this.analyzeElement(childNode as HTMLElement);
-          if (foo.length) {
-            result.push(foo);
-          }
-          break;
-        }
-        case 'mo': {
-          let foo = this.analyzeElement(childNode as HTMLElement);
-          if (foo.length) {
-            result.push(foo);
-          }
-          break;
-        }
+        case 'mn': { break; }
+        case 'mi': { break; }
+        case 'mo': { break; }
       }
     });
     return result.join('');
   }
 
-  private setCopyButton(copyTarget: string): boolean {
+  private setCopyButton(): boolean {
     try {
       const CopyButtonClassName = 'input-output-copier';
       let copyButton = document.createElement('div');
       copyButton.innerText = 'copy problem';
       copyButton.className = CopyButtonClassName;
-      copyButton.addEventListener('click', () => this.textCopyToClipBoard(copyTarget));
+      copyButton.addEventListener('click', () => this.textCopyToClipBoard(this.getProblem()));
       const ProblemStatementElement = document.getElementsByClassName(this.ProblemStatementId)[0];
-      ProblemStatementElement.insertBefore(copyButton, ProblemStatementElement.children[0]);
+      ProblemStatementElement.parentNode.insertBefore(copyButton, ProblemStatementElement);
       return true;
     } catch (err) {
       console.error(err);
